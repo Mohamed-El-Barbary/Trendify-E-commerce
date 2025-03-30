75006fbe7dfee1d6209a21fdac640567dc6c0111
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2025-03-30 17:10:02 +0200

    Update wishlist

--- src/app/pages/allorders/allorders.component.ts
+++ src/app/pages/allorders/allorders.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../../core/services/auth/auth.service';
 import { IUser } from '../../shared/interfaces/iuser';
@@ -9,6 +10,7 @@ import { BreadcrumbModule } from 'primeng/breadcrumb';
 import { MenuItem } from 'primeng/api';
 import { NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { WishlistService } from '../../core/services/wishlist/wishlist.service';
 
 @Component({
   selector: 'app-allorders',
@@ -25,6 +27,7 @@ import { RouterLink } from '@angular/router';
 export class AllordersComponent implements OnInit {
   private readonly authService = inject(AuthService);
   private readonly ordersService = inject(OrdersService);
+  private readonly wishlistService = inject(WishlistService);
   userDataId: IUser = {} as IUser;
   userOrderData: IOrders[] = [];
   latestOrder: IOrders | null = null;
@@ -37,6 +40,15 @@ export class AllordersComponent implements OnInit {
   initializeData(): void {
     this.getAllUserOrders();
     this.links = [{ label: 'Home', route: '/home' }, { label: 'All Orders' }];
+
+    this.wishlistService.getLoggedUserWishlist().subscribe({
+      next: (res) => {
+        this.wishlistService.wishListNumber.set(res.count);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+      },
+    });
   }
 
   getAllUserOrders(): void {
