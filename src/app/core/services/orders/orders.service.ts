f7962b653f5f8fa1034ba558eac15c70637f433c
Mohamed-El-Barbary <mohamedelbarbary511@gmail.com>
2025-03-30 16:59:29 +0200

    New Update Url

--- src/app/core/services/orders/orders.service.ts
+++ src/app/core/services/orders/orders.service.ts
@@ -21,9 +21,7 @@ export class OrdersService {
   }
   
   createOnlineOrder(id:string , data:object):Observable<any>{
-    return this.httpClient.post(`${environment.baseUrl}/api/v1/orders/checkout-session/${id}?url=http://localhost:4200`
-          // rng serve --proxy-config proxy.conf.jsoneturn this.httpClient.post(`${environment.baseUrl}/api/v1/orders/checkout-session/${id}?url=https://trendify-e-commerce-blond.vercel.app/` 
-            , 
+    return this.httpClient.post(`${environment.baseUrl}/api/v1/orders/checkout-session/${id}?url=https://trendify-e-commerce-blond.vercel.app/`, 
       {
         "shippingAddress" : data 
       }
